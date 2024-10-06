af3b732d930c384ff0c6fb971129df381a157d06
Prabesh <tandukarprabesh@gmail.com>
2024-10-06 23:54:08 +1300

    update

--- 5thAttemp/CF/client/src/App.js
+++ 5thAttemp/CF/client/src/App.js
@@ -7,7 +7,7 @@ import CountdownTimer from "./CountdownTimer";
 import ProgressBar from "./ProgressBar";
 import UserDashboard from "./UserDashboard";
 
-const contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 const categories = [
   "Technology",
